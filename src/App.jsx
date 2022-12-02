741cf74293744426e2ce89d78c22d64d2ea2c81a
success ibekwe <Successibekwe047@gmail.com>
2022-12-02 21:30:12 +0100

    feat: Created SignUp, SignIn, and account page

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,24 @@
 import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
+import { AuthContextProvider } from "./context/AuthContext";
 import Home from "./pages/Home";
+import SignUp from "./pages/SignUp";
+import LogIn from "./pages/LogIn";
+import Account from "./pages/Account";
 
 function App() {
   return (
     <>
-      <Navbar />
+      <AuthContextProvider>
+        <Navbar />
 
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<LogIn />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/account" element={<Account />} />
+        </Routes>
+      </AuthContextProvider>
     </>
   );
 }
