fc46758c66e6e950c7d2726fa7d721835ae4d341
success ibekwe <Successibekwe047@gmail.com>
2022-12-02 18:24:33 +0100

    added scroll bar for the movie rows

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -32,7 +32,7 @@ const Main = () => {
         {/* =========================HERO==TEXT==CONTAINER==================================== */}
         <div className="absolute w-full top-[20%] p-4 md:p-8">
           <h1 className="text-3xl md:text-5xl font-bold">{movie?.title}</h1>
-          <div>
+          <div className="my-4">
             <button className="border bg-gray-300 text-blak border-gray-300 py-2 px-5">
               Play
             </button>
